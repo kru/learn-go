716d491475e426ad6fcf8df303b96fa7f005ea61
Kris Pamungkas <kristoforusrp@gmail.com>
2023-10-01 17:10:54 +0700

    small update to counter game

--- core/counter_game.go
+++ core/counter_game.go
@@ -6,10 +6,11 @@ import (
 )
 
 func recCounter(n int64, cnt int) int {
-	cnt++
 	if n == 1 {
-		return cnt - 1
+		return cnt
 	}
+	cnt++
+
 	y := 0
 	for int64(math.Pow(2, float64(y))) < n {
 		y++
@@ -25,6 +26,7 @@ func recCounter(n int64, cnt int) int {
 	return recCounter(n, cnt)
 }
 
+// https://www.hackerrank.com/challenges/one-month-preparation-kit-counter-game/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-two
 func CounterGame(n int64) string {
 	if n == 1 {
 		return "Richard"
