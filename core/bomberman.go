62f7e543f8c1eea25419b5080eda236d5bf08123
Kris Pamungkas <kristoforusrp@gmail.com>
2023-10-11 16:13:13 +0700

    update more bomberman case

--- core/bomberman.go
+++ core/bomberman.go
@@ -1,6 +1,9 @@
 package core
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var direction = [][]int{
 	{0, 1},
@@ -9,55 +12,102 @@ var direction = [][]int{
 	{-1, 0},
 }
 
-func BomberMan(n int32, grid []string) []string {
-	grid01 := make([][]rune, len(grid))
+func isPrime(n int32) bool {
+	if n <= 1 {
+		return false
+	}
 
+	i := int32(2)
+	for n > 2 {
+		if n%i == 0 {
+			return false
+		}
+		i++
+		n--
+	}
+	return true
+}
+
+func factor(n int32) int32 {
+	for i := int32(3); i < 1000; i++ {
+		if isPrime(i) && n%i == 1 {
+			return i
+		}
+	}
+	return n
+}
+
+func plantStrBombs(grid []string) []string {
+	for row := range grid {
+		grid[row] = strings.ReplaceAll(grid[row], ".", "O")
+	}
+	return grid
+}
+
+func plantRuneBoms(grid [][]rune) [][]rune {
+	for i := range grid {
+		for j := range grid[i] {
+			grid[i][j] = 'O'
+		}
+	}
+	return grid
+}
+
+// @TODO: Revisit for optimization O(n)
+// This problem not hard, but tricky
+func BomberMan(n int32, grid []string) []string {
+	bigNum := n
 	if n == 1 {
 		return grid
 	}
-	// see if we need to simulate the second using the j % 2 == 0
-	for j := 2; j <= int(n); j++ {
-		if j%2 == 0 {
-			for i := 0; i < len(grid01); i++ {
-				grid01[i] = make([]rune, len(grid[i]))
 
-				for j := range grid01[i] {
-					grid01[i][j] = 'O'
-				}
-			}
-		} else {
-			for row := range grid {
-				for col := range grid[row] {
-					if string(grid[row][col]) != "O" {
+	if n%2 == 0 {
+		return plantStrBombs(grid)
+	}
+	n = factor(n)
+	fmt.Println("PRime", n, bigNum)
+	gridI := make([][]rune, len(grid))
+
+	for i := range gridI {
+		gridI[i] = []rune(grid[i])
+	}
+
+	for i := 2; i <= int(n); i++ {
+		if i%2 == 0 {
+			gridI = plantRuneBoms(gridI)
+			continue
+		}
+		for row := range gridI {
+			for col := range gridI[row] {
+				for _, dir := range direction {
+					if string(grid[row][col]) == "." {
 						continue
 					}
-					grid01[row][col] = '.'
-					for _, dir := range direction {
-						x, y := row+dir[0], col+dir[1]
-						if x > len(grid)-1 || y > len(grid[row])-1 || x < 0 || y < 0 {
-							continue
-						}
-						grid01[x][y] = '.'
+					x, y := row+dir[0], col+dir[1]
+					gridI[row][col] = '.'
+					if x < 0 || y < 0 || x > len(grid)-1 || y > len(grid[row])-1 {
+						continue
 					}
+					gridI[x][y] = '.'
 				}
 			}
-			for id, gr := range grid01 {
-				s := ""
-				for idx := range gr {
-					s = s + string(gr[idx])
-				}
-				grid[id] = s
+		}
+		for row := range gridI {
+			s := ""
+			for col := range gridI[row] {
+				s += string(gridI[row][col])
 			}
+			grid[row] = s
 		}
 	}
 
-	for id, gr := range grid01 {
+	for row := range gridI {
 		s := ""
-		for idx := range gr {
-			s = s + string(gr[idx])
+		for col := range gridI[row] {
+			s += string(gridI[row][col])
 		}
-		grid[id] = s
+		grid[row] = s
 	}
-	fmt.Println(grid)
+
 	return grid
 }
