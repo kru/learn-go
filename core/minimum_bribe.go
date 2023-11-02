cbccd38e0a49527f971130a76b430d5dc234ebda
Kris Pamungkas <kristoforusrp@gmail.com>
2023-11-02 18:43:46 +0700

    rename

--- core/minimum_bribe.go
+++ core/minimum_bribe.go
@@ -3,45 +3,64 @@ package core
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/kru/learn-go/core/util"
 )
 
 // https://www.hackerrank.com/challenges/one-month-preparation-kit-new-year-chaos/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-month-preparation-kit&playlist_slugs%5B%5D=one-month-week-three
-// This didn't pass test case input06 and input08 respectively, will revisit later
-func MinimumBribes(queue []int32) int32 {
-	st := time.Now().Nanosecond()
-	var op int32
-
-	for i := 0; i < len(queue)-1; i++ {
+// This didn't pass test case input06 and input08, will revisit later
+func MinimumBribe(queue []int32) int32 {
+	// var op int32
+	d := make(map[int32]int)
+	var small int32
+	for i := 0; i < len(queue); i++ {
 		diff := queue[i] - int32(i+1)
 		if diff > 2 {
 			fmt.Println("Too chaotic")
 			return -1
 		}
 
-		if diff > 0 {
-			op += diff
-		} else if diff <= 0 {
-			for j := i + 1; j <= len(queue)-1; j++ {
-				if queue[i] > queue[j] {
-					op += 1
-				}
-			}
+		if diff > 0 || diff < 0 {
+			// op += 1
+			queue[int(queue[i])-1], queue[i] = queue[i], queue[int(queue[i])-1]
+			d[queue[i]] += 1
+
+			i = i - 1
 		}
+		if diff == 0 {
+			small++
+		}
+
 	}
-	et := time.Now().Nanosecond()
-	fmt.Printf("%d -> it tooks %.2f milisec\n", op, float64(et-st)/math.Pow10(6))
-	return op
+	fmt.Printf("%v\n", d)
+
+	var res int
+	for key := range d {
+		res += d[key]
+	}
+	return int32(res)
 }
 
+// expected output from test case 6
+// 96110
+// Too chaotic
+// Too chaotic
+// 96319
+// 96323
+// 96058
+
+// expected output from test case 8
+// 119814
+// 120175
+// 119810
+// 119827
+// Too chaotic
+
 func InitBribe() {
-	f, err := os.Open("core/custom-input/minimum-bribe-input08")
+	f, err := os.Open("core/custom-input/minimum-bribe-input06")
 	util.CheckError(err)
 
 	reader := bufio.NewReaderSize(f, 16*1024*1024)
@@ -66,7 +85,7 @@ func InitBribe() {
 			q = append(q, qItem)
 		}
 
-		MinimumBribes(q)
+		MinimumBribe(q)
 
 		f.Close()
 	}
