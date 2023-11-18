a94cfc490aae7918557334f265204803eefa162a
Kris Pamungkas <kristoforusrp@gmail.com>
2023-11-18 11:31:03 +0700

    optimize grid challenge

--- core/grid_challenge.go
+++ core/grid_challenge.go
@@ -19,43 +19,34 @@ func (g gri) Swap(i, j int) {
 	g[i], g[j] = g[j], g[i]
 }
 
-func isContigous(list []int32) bool {
-	for i := 0; i < len(list)-1; i++ {
-		if list[i] > list[i+1] {
-			return false
-		}
-	}
-	return true
-}
-
 func GridChallenge(grid []string) string {
 	fmt.Println(grid)
-	var strByte [][]int32
-	columns := make([][]int32, len(grid[0]))
-	for _, str := range grid {
-		tmp := make(gri, len(str))
-		for i, chr := range str {
-			tmp[i] = chr
+	var sortedGrid []string
+	for i := 0; i < len(grid); i++ {
+		var isSorted bool
+		var prev int32
+		tmp := make(gri, len(grid[i]))
+		for j := 0; j < len(grid[i]); j++ {
+			if prev > int32(grid[i][j]) {
+				isSorted = false
+			}
+			tmp[j] = int32(grid[i][j])
+			prev = int32(grid[i][j])
 		}
-		sort.Sort(tmp)
-
-		strByte = append(strByte, tmp)
-
-		for j := range tmp {
-			columns[j] = append(columns[j], tmp[j])
+		if !isSorted {
+			sort.Sort(tmp)
 		}
-
+		sortedGrid = append(sortedGrid, string(tmp))
 	}
 
-	for i := 0; i < len(strByte); i++ {
-		if !isContigous(strByte[i]) {
-			return "NO"
-		}
-	}
+	for k := 0; k < len(sortedGrid[0]); k++ {
+		var prev int32
+		for col := 0; col < len(sortedGrid); col++ {
+			if prev > int32(sortedGrid[col][k]) {
+				return "NO"
+			}
+			prev = int32(sortedGrid[col][k])
 
-	for j := 0; j < len(columns); j++ {
-		if !isContigous(columns[j]) {
-			return "NO"
 		}
 	}
 
